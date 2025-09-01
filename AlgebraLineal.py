881c63677c6e0d74cdb7369b6e3e2d40b46f740a
ctobonpe <ctobonpe@gmail.com>
2025-09-01 10:12:41 -0500

    ecuaciones lineales, autovalores y autovectores

--- AlgebraLineal.py
+++ AlgebraLineal.py
@@ -49,4 +49,41 @@ eigenvalues, eigenvectors = np.linalg.eig(A)
 print("Original Matrix A:\n", A)
 print("\nEigenvalues:", eigenvalues)
 print("\nEigenvectors (columns correspond to eigenvalues):\n", eigenvectors)
+
+
+#solucion a un sistema de ecuaciones lineales
+
+A = np.array([[3,1],[1,2]])
+b = np.array([9,8])
+print("A:\n",A)
+print("b:\n",b)
+
+z = np.linalg.solve(A,b)
+print("z:\n",z)
+print(np.dot(A,x))
+
+#solucion con pseudoinversa
+B = np.array([[1,2],[3,4],[5,6]])
+c = np.array([1,2,3])
+
+print("B:\n",A)
+print("c:\n",c)
+
+d = np.linalg.pinv(B)
+print("d:\n",d)
+
+x = np.dot(d,c)
+print("x:\n",x)
+
+#autovalores y autovectores
+
+C = np.array([[3, 1],[1, 3]])
+
+eigenvalues, eigenvectors = np.linalg.eig(C)
+print("Autovalores:", eigenvalues)
+print("Autovectores:\n", eigenvectors)
+
+
+
+
     
